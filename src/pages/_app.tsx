939256e1e600a057130e03f63202010994a70ca6
Eronheit <aquinoeronaldo@gmail.com>
2021-06-01 23:08:46 -0300

    feat: config default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
+import Head from 'next/head'
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+import GlobalStyle from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
