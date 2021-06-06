239a2e3f4aba3e807d039119409bd6e81f4dcd81
Eronheit <aquinoeronaldo@gmail.com>
2021-06-06 16:14:44 -0300

    chore: add Storybook

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
+
 import Main from '.'
+
 describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
@@ -14,8 +16,6 @@ describe('<Main />', () => {
   it('should render the colors correctly', () => {
     const { container } = render(<Main />)
 
-    expect(container.firstChild).toHaveStyle({
-      'background-color': '#06092b'
-    })
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
